c51f8847a664b822c3d27d1a42f581f131301288
wanying0106 <42138708+wanying0106@users.noreply.github.com>
2019-06-29 14:09:33 +0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-from time import time
-from selenium import webdriver
-
-driver = webdriver.Chrome()
-driver.get('http://www.baidu.com')
-
-driver.find_element_by_id('kw').send_keys("sousuo")
-driver.implicitly_wait(30)
